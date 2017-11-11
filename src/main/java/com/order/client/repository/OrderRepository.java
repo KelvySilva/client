2c31eeedee28f76e62c196af20882457bbcf5b01
Raul Maia <raul.mysterious1@gmail.com>
2017-11-10 22:34:31 -0200

    Alterando pedido

--- src/main/java/com/order/client/repository/OrderRepository.java
+++ src/main/java/com/order/client/repository/OrderRepository.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.order.client.model.Order;
+import com.order.client.model.Orders;
 
-public interface OrderRepository extends JpaRepository<Order, BigDecimal>{
+public interface OrderRepository extends JpaRepository<Orders, BigDecimal>{
 
 }
